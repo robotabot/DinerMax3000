ee9360ed48c29afda43b2d8bf74b17dc40823407
lmortenson <robotabot@gmail.com>
2015-03-09 21:24:15 -0400

    demonstration code

--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -10,6 +10,13 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
+            Menu summerMenu = new Menu();
+            summerMenu.Name = "Summer menu";
+            MenuItem mostaciolli = new MenuItem();
+            mostaciolli.Title = "Mostaciolli";
+            mostaciolli.Description = "Noodles, Sauce, Cheese, Oh My!";
+            mostaciolli.Price = 12.50;
+            summerMenu.Items.Add(mostaciolli);
         }
     }
 }
